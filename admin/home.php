17c22a47380baa5c3db800cfe6c9296edc0a1ff9
techworldthink <jobin25051999@gmail.com>
2021-02-16 16:14:43 +0530

    admin dashboared started

---
+++ admin/home.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Page</title>
+	<!-- Bootstrap -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+	<!-- my style -->
+	<link href="library/home.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+	<div id="mySidenav" class="sidenav">
+		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		<a href="#">Add Book</a>
+		<a href="#">Add Student</a>
+		<a href="#">Search Book</a>
+		<a href="#">Logout</a>
+	
+	</div>
+	<!-- Use any element to open the sidenav -->
+	<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="library/javascript.js"></script>
+</body>
+</html>
